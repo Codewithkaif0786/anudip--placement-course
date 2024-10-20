8bc701057b2f576f73d813c662312eeb2cbb0ec9
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2024-10-20 20:14:59 +0530

    Create condidates.java

---
+++ condidates.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class assingment2 {
+  public static void main(String[] args) {
+    String condidates[] = {"Saif", "Kaif", "Arbaz", "Kais", "Kuzzu"};
+    System.out.println("Condidates are :");
+    for(int i=0; i<condidates.length; i++) {
+     System.out.println(condidates[i]);
+    }
+  }
+}
