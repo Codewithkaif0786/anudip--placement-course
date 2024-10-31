c214c25e03254349f97b6341f067a98af417b2b8
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2024-10-31 15:50:51 +0530

    Create leapYearChe.java

---
+++ leapYearChe.java
@@ -0,0 +1,15 @@
+package anudip_exercise;
+
+import java.util.Scanner;
+
+public class lab1 {
+    public static void main(String[] args) {
+       int n = 1999;
+
+       if(n % 4 == 0) {
+        System.out.println("Leap year!");
+       } else {
+        System.out.println("Not a leap year!");
+       }
+    }
+}
